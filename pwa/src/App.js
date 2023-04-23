bd5f7dd5fa90aac1941f4e2541b8f0bb313ff396
kun-sys <73662477+kun-sys@users.noreply.github.com>
2023-04-23 13:51:11 +0200

    app
    app.js para modificar
    
    Co-Authored-By: emptywhity <99326402+emptywhity@users.noreply.github.com>

--- pwa/src/App.js
+++ pwa/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const autor = document.getElementById('autor').value;
+    const titulo = document.getElementById('titulo').value;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${autor}+intitle:${titulo}`;
+    axios.get(url)
+      .then(response => {
+        const books = response.data.items;
+        this.setState({ books });
+      })
+      .catch(error => console.error(error));
+  }
+  render() {
+    const { books } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Bienvenido a mi buscador de libro</p>
+        </header>
+        <body>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label htmlFor="autor">Autor:</label>
+              <input type="text" id="autor" placeholder="Introduzca el nombre del autor" />
+            </div>
+            <div>
+              <label htmlFor="titulo">Título:</label>
+              <input type="text" id="titulo" placeholder="Introduzca el título del libro" />
+            </div>
+            <button type="submit">Buscar</button>
+          </form>
+          <ul>
+            {books.map(book => (
+              <li key={book.id}>{book.volumeInfo.title}</li>
+            ))}
+          </ul>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
